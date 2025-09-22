7ffc282c11f5478fa8d3c07468354014307f090e
brian <brian@example.com>
2025-09-22 09:14:40 -0400

    add some type structure

---
+++ src/game/Card.ts
@@ -0,0 +1,54 @@
+enum Suit {
+    HEART = "HEART", // red, human
+    DIAMOND = "DIAMOND", // blue, gnome
+    SHOVEL = "SHOVEL", // gray, dwarf
+    ARROWHEAD = "ARROWHEAD", // green, elf
+    CLUB = "CLUB", // brown, orc
+
+    // maybe
+    SKULL = "SKULL", // black, undead
+    STAR = "STAR", // yellow, celestial
+    CIRCLE = "CIRCLE", // purple, demon
+    TRIANGLE = "TRIANGLE", // orange, beast
+    SQUARE = "SQUARE", // pink, fairy
+}
+
+enum Bonus {
+    ENRICHED = "ENRICHED",
+    WILD = "WILD",
+    POINTY = "POINTY",
+    STURDY = "STURDY",
+}
+
+enum SpellType {
+    SUMMON = "SUMMON",
+    ENCHANTMENT = "ENCHANTMENT",
+    CONJURATION = "CONJURATION",
+}
+
+interface CardInterface {
+    suit: Suit;
+    value: number;
+    bonus?: Bonus;
+
+    // optional properties for spell cards
+    name?: string;
+    description?: string;
+    type?: SpellType;
+    cast?: () => void;
+}
+
+export default class Card implements CardInterface {
+    suit: Suit;
+    value: number;
+
+    name?: string;
+    description?: string;
+    type?: SpellType;
+    cast?: () => void;
+
+    constructor(suit: Suit, value: number) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
---
+++ src/game/Enums.ts
@@ -0,0 +1,8 @@
+
+export enum Race {
+    HUMAN = "HUMAN",
+    GNOME = "GNOME",
+    DWARF = "DWARF",
+    ELF = "ELF",
+    ORC = "ORC",
+}
